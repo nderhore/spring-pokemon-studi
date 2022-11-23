42357ee8b9a9d95f267fdb1df08c0e01b07c1b10
Nathan Derhore <nathan@derhore.fr>
2022-11-23 18:55:36 +0100

    first project

---
+++ src/test/java/com/pokemon/studi/StudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.studi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
